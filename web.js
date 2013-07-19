cbbf308dce9a20e535c238c0d17ef1365dda6066
Ubuntu <ubuntu@ip-172-31-47-49.us-west-2.compute.internal>
2013-07-19 07:32:39 +0000

    updated to use the bootstrap

--- web.js
+++ web.js
@@ -3,12 +3,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/bootstrap'));
+
 app.get('/', function(request, response) {
   var buf = fs.readFileSync('index.html');
   response.send(buf.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
